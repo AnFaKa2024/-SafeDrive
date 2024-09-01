aac1cfe350eab538493395828c4333686f31fcc5
kmarq <kmarqueskawahira@hotmail.com.br>
2024-09-01 10:08:46 -0300

    Arquivo e Abas Criados

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,15 @@
+export default function Cabecalho(){
+
+  return(
+    <header>
+
+      <img src="Imagem/LG_SafeDrive.png" alt="LogoPorto" />
+
+      <a href="/Paginas/login.html"><img src="Imagem/icone_cliente_branco.png" alt="IconeCliente"/>
+      </a>
+
+    </header>
+  )
+
+
+}
