46903a6847f33cb7d9592f38264c511087863446
kmarq <kmarqueskawahira@hotmail.com.br>
2024-09-15 01:16:51 -0300

    Foram criadas paginas de formularios e importado os routes e styled

---
+++ src/routes/CadMecanico/index.tsx
@@ -0,0 +1,20 @@
+
+import CadMeca from "./CadMeca";
+import CadOficina from "./CadOficina";
+
+
+
+export default function CadMecanico(){
+
+  return(
+
+    <>
+    <h1>Seja um de nossos parceiros</h1>
+    <form action="cadastro mecanico e oficina" method="get">
+      <CadMeca/>
+      <CadOficina/>
+    </form>
+    </>
+    
+    )
+}
---
+++ src/styled.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+
+export const DivMec = styled.div`
+   border: 3px solid #2e0b7d;
+   margin: 1px;
+   margin-left: 12.5vw;
+   padding: 5px;
+   background-color: #bdbdc095;
+   font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+   font-size: 1.05em; 
+   text-align: center;
+   width: 70vw;
+   line-height: 1.7;
+
+  
+   .idEnd{
+    padding: 20vw;
+    
+
+   }
+
+   button{
+    display: inline-block;
+    /*ver como faz para ficar com espaço em volta dele*/
+    padding: 5px 10px;
+    background-color: #2e0b7d;
+    color: white;
+  }
+`
+
+export const DivOficina = styled.ul`
+  list-style: none;
+
+`
+
+export const MainHistorico = styled.main`
+  text-align: center;
+
+  p{
+    font-size: 1.2em;
+  }
+
+
+`
